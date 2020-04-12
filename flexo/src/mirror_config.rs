0bb96cd612a161e268fa24e012415a27a6ffb53c
Fabian Muscariello <nroi@mailbox.org>
2020-04-12 12:12:06 +0200

    Allow configuration via environment variables
    Closes #1

--- flexo/src/mirror_config.rs
+++ flexo/src/mirror_config.rs
@@ -46,8 +46,76 @@ pub struct MirrorConfig {
     pub mirrors_auto: MirrorsAutoConfig,
 }
 
-pub fn load_config() -> MirrorConfig {
+fn load_toml_config() -> MirrorConfig {
     let config_contents = fs::read_to_string(CONFIG_FILE)
         .unwrap_or_else(|_| panic!("Unable to read file: {}", CONFIG_FILE));
     toml::from_str(&config_contents).unwrap()
+}
+
+#[derive(Deserialize)]
+struct DValue <T> {
+    value: T
+}
+
+fn parse_env_toml<T>(s: &str) -> Option<T> where
+          T: serde::de::DeserializeOwned,
+{
+    let env_var = std::env::var(s).ok()?;
+    // Our actual intent is to parse the environment variable as a TOML value, but the parser accepts only complete
+    // TOML documents with key-value pairs. So we construct a TOML document with a single key-value pair, and
+    // then extract the value.
+    let toml_document: String = "value = ".to_owned() + &env_var;
+    let deserialized = toml::from_str::<DValue<T>>(&toml_document).unwrap();
+    Some(deserialized.value)
+}
+
+fn mirrors_auto_config_from_env() -> MirrorsAutoConfig {
+    let https_required = parse_env_toml::<bool>("FLEXO_MIRRORS_AUTO_HTTPS_REQUIRED").unwrap();
+    let ipv4 = parse_env_toml::<bool>("FLEXO_MIRRORS_AUTO_IPV4").unwrap();
+    let ipv6 = parse_env_toml::<bool>("FLEXO_MIRRORS_AUTO_IPV6").unwrap();
+    let max_score = parse_env_toml::<f64>("FLEXO_MIRRORS_AUTO_MAX_SCORE").unwrap();
+    let num_mirrors = parse_env_toml::<usize>("FLEXO_MIRRORS_AUTO_NUM_MIRRORS").unwrap();
+    let mirrors_random_or_sort = parse_env_toml::<MirrorsRandomOrSort>("FLEXO_MIRRORS_AUTO_MIRRORS_RANDOM_OR_SORT")
+        .unwrap();
+    let timeout = parse_env_toml::<u64>("FLEXO_MIRRORS_AUTO_TIMEOUT").unwrap();
+    let low_speed_limit = parse_env_toml::<u32>("FLEXO_MIRRORS_AUTO_LOW_SPEED_LIMIT");
+    let low_speed_time_secs = parse_env_toml::<u64>("FLEXO_MIRRORS_AUTO_LOW_SPEED_TIME_SECS");
+    let max_speed_limit = parse_env_toml::<u64>("FLEXO_MIRRORS_AUTO_MAX_SPEED_LIMIT");
+    MirrorsAutoConfig {
+        https_required,
+        ipv4,
+        ipv6,
+        max_score,
+        num_mirrors,
+        mirrors_random_or_sort,
+        timeout,
+        low_speed_limit,
+        low_speed_time_secs,
+        max_speed_limit,
+    }
+}
+
+fn mirror_config_from_env() -> MirrorConfig {
+    let mirrors_auto = mirrors_auto_config_from_env();
+    let cache_directory = parse_env_toml::<String>("FLEXO_CACHE_DIRECTORY").unwrap();
+    let port = parse_env_toml::<u16>("FLEXO_PORT").unwrap();
+    let mirror_selection_method = parse_env_toml::<MirrorSelectionMethod>("FLEXO_MIRROR_SELECTION_METHOD").unwrap();
+    let mirrors_predefined = parse_env_toml::<Vec<String>>("FLEXO_MIRRORS_PREDEFINED").unwrap();
+    let mirrors_blacklist = parse_env_toml::<Vec<String>>("FLEXO_MIRRORS_BLACKLIST").unwrap();
+    MirrorConfig {
+        cache_directory,
+        port,
+        mirror_selection_method,
+        mirrors_predefined,
+        mirrors_blacklist,
+        mirrors_auto
+    }
+}
+
+pub fn load_config() -> MirrorConfig {
+    if std::env::vars().any(|(key, _value)| key.starts_with("FLEXO_")) {
+        mirror_config_from_env()
+    } else {
+        load_toml_config()
+    }
 }
