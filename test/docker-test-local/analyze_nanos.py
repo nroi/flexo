0f7c792b06f34386cbe77a641d6b1bf135d86c3d
Fabian Muscariello <nroi@mailbox.org>
2020-12-06 17:08:55 +0100

    Move some files

--- test/docker-test-local/analyze_nanos.py
+++ test/docker-test-local/analyze_nanos.py
@@ -3,11 +3,11 @@
 import fileinput
 import sys
 import re
+import datetime
 
 tolerance = .35
 
 prev = None
-
 
 for line in fileinput.input():
     if re.match('^\d+$', line) is not None:
@@ -15,7 +15,8 @@ for line in fileinput.input():
         if prev is not None:
             diff = (current - prev) / 1_000_000_000
             if diff > tolerance:
-                print('>>> Threshold exceeded: ' + str(diff))
+                timestamp = str(datetime.datetime.now())
+                print('>>> Threshold exceeded at ' + timestamp + ': ' + str(diff))
         prev = current
     else:
         print(line, end='')
