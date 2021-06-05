b594f26f2d700464e85a0d15fc91102c6312daa6
Fabian Muscariello <nroi@mailbox.org>
2021-06-05 18:34:09 +0200

    Add TODO

--- flexo/src/mirror_fetch.rs
+++ flexo/src/mirror_fetch.rs
@@ -199,7 +199,7 @@ pub fn measure_latency(url: &str, timeout: Duration) -> Result<MirrorResults, cu
     easy.url(&url)?;
     easy.nobody(true)?;
     easy.follow_location(true)?;
-    easy.dns_cache_timeout(Duration::from_secs(3600 * 24))?;
+    easy.dns_cache_timeout(Duration::from_secs(3600 * 24))?; // TODO avoid hardcoded values
     easy.timeout(timeout)?;
     // we use httparse to parse the headers, but httparse doesn't support HTTP/2 yet. HTTP/2 shouldn't provide
     // any benefit for our use case (afaik), so this setting should not have any downsides.
