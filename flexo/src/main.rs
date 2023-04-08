9878de67f85452d693a018b30e8ed226f7aaa34c
Fabian Muscariello <nroi@mailbox.org>
2023-04-08 13:18:04 +0200

    Introduce http_headers.rs

--- flexo/src/main.rs
+++ flexo/src/main.rs
@@ -29,6 +29,7 @@ use tempfile::tempfile;
 
 use flexo::*;
 use mirror_flexo::*;
+use crate::http_headers::{PayloadOrigin, redirect_header, reply_header_bad_request, reply_header_forbidden, reply_header_internal_server_error, reply_header_not_found, reply_header_partial, reply_header_success};
 
 use crate::mirror_cache::{DemarshallError, TimestampedDownloadProviders};
 use crate::mirror_config::{CustomRepo, MirrorConfig, MirrorSelectionMethod};
@@ -53,13 +54,6 @@ const MAX_SENDFILE_COUNT: usize = 0x7fff_f000;
 const MAX_SENDFILE_COUNT: usize = 128;
 
 const TIMEOUT_RECEIVE_CONTENT_LENGTH: Duration = Duration::from_secs(7);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-enum PayloadOrigin {
-    Cache,
-    RemoteMirror,
-    NoPayload,
-}
 
 fn main() {
     env_logger::builder().format_timestamp_millis().init();
@@ -809,75 +803,6 @@ fn serve_200_ok_body(client_stream: &mut TcpStream, body: &[u8]) -> io::Result<(
     client_stream.write_all(body)
 }
 
-fn reply_header_success(content_length: u64, payload_origin: PayloadOrigin) -> String {
-    reply_header("200 OK", content_length, None, payload_origin, SystemTime::now())
-}
-
-fn reply_header_partial(content_length: u64, resume_from: u64, payload_origin: PayloadOrigin) -> String {
-    reply_header(
-        "206 Partial Content", content_length, Some(resume_from), payload_origin, SystemTime::now()
-    )
-}
-
-fn reply_header_not_found() -> String {
-    reply_header("404 Not Found", 0, None, PayloadOrigin::NoPayload, SystemTime::now())
-}
-
-fn reply_header_bad_request() -> String {
-    reply_header("400 Bad Request", 0, None, PayloadOrigin::NoPayload, SystemTime::now())
-}
-
-fn reply_header_internal_server_error() -> String {
-    reply_header("500 Internal Server Error", 0, None, PayloadOrigin::NoPayload, SystemTime::now())
-}
-
-fn reply_header_forbidden() -> String {
-    reply_header("403 Forbidden", 0, None, PayloadOrigin::NoPayload, SystemTime::now())
-}
-
-fn reply_header(
-    status_line: &str,
-    content_length: u64,
-    resume_from: Option<u64>,
-    payload_origin: PayloadOrigin,
-    now: SystemTime,
-) -> String {
-    let timestamp = httpdate::fmt_http_date(now);
-    let content_range_header = resume_from.map(|r| {
-        let complete_size = content_length + r;
-        let last_byte = complete_size - 1;
-        format!("Content-Range: bytes {}-{}/{}\r\n", r, last_byte, complete_size)
-    }).unwrap_or_else(|| "".to_owned());
-    let header = format!("\
-        HTTP/1.1 {}\r\n\
-        Server: flexo\r\n\
-        Date: {}\r\n\
-        Flexo-Payload-Origin: {:?}\r\n\
-        {}\
-        Content-Length: {}\r\n\r\n",
-                         status_line,
-                         timestamp,
-                         payload_origin,
-                         content_range_header,
-                         content_length
-    );
-    debug!("Sending header to client: {:?}", &header);
-
-    header
-}
-
-fn redirect_header(path: &str, now: SystemTime) -> String {
-    let timestamp = httpdate::fmt_http_date(now);
-    let header = format!("\
-        HTTP/1.1 301 Moved Permanently\r\n\
-        Server: flexo\r\n\
-        Date: {}\r\n\
-        Content-Length: 0\r\n\
-        Location: {}\r\n\r\n", timestamp, path);
-
-    header
-}
-
 fn serve_from_complete_file(
     mut file: File,
     resume_from: Option<u64>,
@@ -978,30 +903,4 @@ fn custom_provider_from_request_test() {
 
     assert_eq!(provider, Some(expected_provider));
     assert_eq!(new_get_request, expected_get_request);
-}
-
-#[test]
-fn test_reply_header() {
-    let timestamp = httpdate::parse_http_date("Thu, 06 Apr 2023 20:00:18 GMT").unwrap();
-    let expected = "HTTP/1.1 OK\r\n\
-        Server: flexo\r\n\
-        Date: Thu, 06 Apr 2023 20:00:18 GMT\r\n\
-        Flexo-Payload-Origin: NoPayload\r\n\
-        Content-Length: 0\r\n\r\n";
-    let actual = reply_header("OK", 0, None, PayloadOrigin::NoPayload, timestamp);
-
-    assert_eq!(expected, actual)
-}
-
-#[test]
-fn test_redirect_header() {
-    let timestamp = httpdate::parse_http_date("Thu, 06 Apr 2023 20:00:18 GMT").unwrap();
-    let expected = "HTTP/1.1 301 Moved Permanently\r\n\
-        Server: flexo\r\n\
-        Date: Thu, 06 Apr 2023 20:00:18 GMT\r\n\
-        Content-Length: 0\r\n\
-        Location: /new/location\r\n\r\n";
-    let actual = redirect_header("/new/location", timestamp);
-
-    assert_eq!(expected, actual)
 }
